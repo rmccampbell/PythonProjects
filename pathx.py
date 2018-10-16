4b3fa7abe6f1016f0f37ead9c8cb944e621d2940
Ryan McCampbell <rmccampbell7@gmail.com>
2018-10-16 16:52:34 -0400

    updated pathx

--- pathx.py
+++ pathx.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
-import sys, os, argparse, ctypes
+import sys, argparse, ctypes
+import os.path as op
 from winreg import *
 
-SendMessage = ctypes.windll.user32.SendMessageW
-##SendMessageTimeout = ctypes.windll.user32.SendMessageTimeoutW
+##SendMessage = ctypes.windll.user32.SendMessageW
+SendMessageTimeout = ctypes.windll.user32.SendMessageTimeoutW
 HWND_BROADCAST = 0xffff
 WM_SETTINGCHANGE = 0x1a
-##SMTO_NORMAL = 0
+SMTO_NORMAL = 0
 
 def main(add=None, index=None, remove=None, contains=(), system=False,
          lines=False):
     if system:
         key = HKEY_LOCAL_MACHINE
-        subkey = 'SYSTEM\CurrentControlSet\Control\Session Manager\Environment'
+        subkey = r'SYSTEM\CurrentControlSet\Control\Session Manager\Environment'
     else:
         key, subkey = HKEY_CURRENT_USER, 'Environment'
-    if add or remove:
+    if add is not None or remove is not None:
         mode = KEY_ALL_ACCESS
     else:
         mode = KEY_READ
@@ -23,36 +24,37 @@ def main(add=None, index=None, remove=None, contains=(), system=False,
         value, typ = QueryValueEx(key, 'Path')
         values = value.split(';')
         if contains:
+            values = {op.normcase(op.normpath(op.expandvars(val)))
+                      for val in values}
             for path in contains:
-                if path in values:
+                if op.normcase(op.abspath(path)) in values:
                     print('"%s" in path' % path)
                 else:
                     print('"%s" not in path' % path)
             return
-        if remove:
+        if remove is not None:
             if remove not in values:
                 print('"%s" not in path' % remove)
                 return
             values.remove(remove)
-        if add:
+        if add is not None:
             if add in values:
                 print('"%s" already in path' % add)
                 return
             if index is None:
                 index = len(values)
             values.insert(index, add)
-        if add or remove:
+        if add is not None or remove is not None:
             value = ';'.join(values)
             typ = REG_EXPAND_SZ if value.count('%') >= 2 else REG_SZ
             SetValueEx(key, 'Path', 0, typ, value)
-            SendMessage(HWND_BROADCAST, WM_SETTINGCHANGE, 0, 'Environment')
-##            SendMessageTimeout(HWND_BROADCAST, WM_SETTINGCHANGE, 0,
-##                               'Environment', SMTO_NORMAL, 0, 0)
+##            SendMessage(HWND_BROADCAST, WM_SETTINGCHANGE, 0, 'Environment')
+            SendMessageTimeout(HWND_BROADCAST, WM_SETTINGCHANGE, 0,
+                               'Environment', SMTO_NORMAL, 100, 0)
             print('** Registry updated **')
-    if not contains:
-        if lines:
-            value = value.replace(';', '\n')
-        print(value)
+    if lines:
+        value = value.replace(';', '\n')
+    print(value)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
