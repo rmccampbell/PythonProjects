bf7ec75b505d4bdc2597b7c3e647f9c654be17a2
Ryan McCampbell <rmccampbell7@gmail.com>
2019-06-22 17:50:36 -0700

    updated repr

--- repr.py
+++ repr.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 import sys
 
-file = sys.stdin.buffer
+unicode = False
+if '-u' in sys.argv:
+    unicode = True
+    sys.argv.remove('-u')
+
 if len(sys.argv) > 1:
-    file = open(sys.argv[1], 'rb')
-print(repr(file.read())[1:])
+    mode = 'r' if unicode else 'rb'
+    encoding = 'utf-8' if unicode else None
+    file = open(sys.argv[1], mode, encoding=encoding)
+else:
+    file = sys.stdin if unicode else sys.stdin.buffer
+
+print(repr(file.read()).lstrip('b'))
