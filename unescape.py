f5627f45e54023403d83ba0618b8be114ec87b86
Ryan McCampbell <rmccampbell7@gmail.com>
2019-08-13 19:05:53 -0700

    updated unescape.py

--- unescape.py
+++ unescape.py
@@ -2,16 +2,20 @@
 import sys, argparse
 
 p = argparse.ArgumentParser()
-p.add_argument('args', nargs='*')
+p.add_argument('input', nargs='*')
 p.add_argument('-b', '--binary', action='store_true')
 p.add_argument('-u', '--utf8', action='store_true')
+p.add_argument('-n', '--nonewline', action='store_true')
 args = p.parse_args()
 
-inpt = ' '.join(args.args)
-unescaped = inpt.encode('latin-1', 'backslashreplace').decode('unicode_escape')
-if args.binary:
-    sys.stdout.buffer.write(unescaped.encode('latin-1'))
-elif args.utf8:
-    sys.stdout.buffer.write(unescaped.encode('utf-8'))
-else:
-    sys.stdout.write(unescaped)
+for inpt in args.input:
+    unescaped = inpt.encode('latin-1', 'backslashreplace').decode('unicode_escape')
+    if args.binary:
+        sys.stdout.buffer.write(unescaped.encode('latin-1'))
+    elif args.utf8:
+        sys.stdout.buffer.write(unescaped.encode('utf-8'))
+    else:
+        sys.stdout.write(unescaped)
+
+    if not args.nonewline:
+        print(flush=True)
