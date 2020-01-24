cb70edde5b23a158a38d3974ec91795eeb30b15d
Ryan McCampbell <rmccampbell7@gmail.com>
2020-01-23 23:38:51 -0500

    waves.pyw

--- waves.py
+++ waves.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
 import sys
 import math
+import argparse
 import numpy as np
 import pygame as pg
 # import pygame.gfxdraw
 
+# TODO: Add drawable barriers
+
 FPS = 40
 
 PIX = 2
-WIDTH = 300
+WIDTH = 350
 HEIGHT = 300
 WRAP = False
+DAMP_EDGE = 0
 
 K = 0.2
 DAMPING = .002
+EDGE_DAMPING = .2
 MAXVAL = 10
 MINVAL = -10
 DIAG = .4
@@ -22,28 +27,29 @@ BALLACCEL = 1
 
 COLOR0 = np.array([255, 255, 255])
 COLOR1 = np.array([0, 0, 255])
-BALLCOLOR = (255, 0, 0)
+BALLCOLOR = np.array([255, 0, 0])
 
-def propagate(z, v, wrap=False):
+def propagate(z, v, damping, wrap=WRAP):
     pad = np.pad(z, 1, 'wrap' if wrap else 'constant')
-    a = (pad[:-2, 1:-1] + pad[2:, 1:-1] + pad[1:-1, :-2] + pad[1:-1, 2:]
-         + (pad[:-2, :-2] + pad[2:, :-2] + pad[2:, 2:] + pad[:-2, 2:]) * DIAG
-         - (4 + 4*DIAG) * z) * K - v * DAMPING
+    a = (
+        pad[:-2, 1:-1] + pad[2:, 1:-1] + pad[1:-1, :-2] + pad[1:-1, 2:]
+        + (pad[:-2, :-2] + pad[2:, :-2] + pad[2:, 2:] + pad[:-2, 2:]) * DIAG
+        - (4 + 4*DIAG) * z
+    )*K - v*damping
     v += a
     z += v
 
-def update_balls(z, balls, wrap=False):
-    dzdx, dzdy = np.gradient(z * BALLACCEL)
-    for i, (x, y) in enumerate(balls):
-        xind = math.floor(np.clip(x, 0, WIDTH-1))
-        yind = math.floor(np.clip(y, 0, HEIGHT-1))
-        x += dzdx[xind, yind]
-        y += dzdy[xind, yind]
+def update_balls(z, balls, wrap=WRAP):
+    gradz = np.dstack(np.gradient(z * BALLACCEL))
+    size = np.array(z.shape)
+    for i, xy in enumerate(balls):
+        xind, yind = np.clip(xy.astype(int), 0, size-1)
+        xy += gradz[xind, yind]
         if wrap:
-            x, y = x % WIDTH, y % HEIGHT
+            xy %= size
         else:
-            x, y = np.clip(x, 0, WIDTH), np.clip(y, 0, HEIGHT)
-        balls[i] = (x, y)
+            xy = np.clip(xy, 0, size)
+        balls[i] = xy
 
 def interp_colors(t, c0, c1):
     color = t[..., np.newaxis] * (c1 - c0) + c0
@@ -67,7 +73,7 @@ def splash(z, v, down, px, py):
     v[x, y] += -SPLASH if down else SPLASH
 
 def add_ball(balls, px, py):
-    balls.append((px // PIX, py // PIX))
+    balls.append(np.array([px / PIX, py / PIX], float))
 
 def noise(width, height):
     return np.random.normal(0, .5, (width, height))
@@ -77,13 +83,20 @@ def clear(z, v, balls):
     z[:] = noise(*z.shape)
     balls.clear()
 
-def main():
+def main(width=WIDTH, height=HEIGHT, wrap=WRAP, damp_edge=DAMP_EDGE):
     wrap = '-w' in sys.argv[1:] or WRAP
-    z = np.zeros((WIDTH, HEIGHT))
-    v = np.zeros((WIDTH, HEIGHT))
+    z = np.zeros((width, height))
+    v = np.zeros((width, height))
+
+    damping = DAMPING
+    if damp_edge:
+        ramp = np.pad(np.zeros(np.array(z.shape)-2*damp_edge), damp_edge,
+                      mode='linear_ramp', end_values=1)
+        damping = ramp**2*(EDGE_DAMPING - DAMPING) + DAMPING
+
     balls = []
     clear(z, v, balls)
-    screen = pg.display.set_mode((PIX*WIDTH, PIX*HEIGHT))
+    screen = pg.display.set_mode((PIX*width, PIX*height))
     draw(screen, z)
     clock = pg.time.Clock()
     running = True
@@ -108,7 +121,7 @@ def main():
             splash(z, v, mouse[0], *pg.mouse.get_pos())
 
         clock.tick(FPS)
-        propagate(z, v, wrap)
+        propagate(z, v, damping, wrap)
         draw(screen, z)
         if balls:
             update_balls(z, balls, wrap)
@@ -116,7 +129,13 @@ def main():
         pg.display.flip()
 
 if __name__ == '__main__':
+    p = argparse.ArgumentParser()
+    p.add_argument('width', nargs='?', type=int, default=WIDTH)
+    p.add_argument('height', nargs='?', type=int, default=HEIGHT)
+    p.add_argument('-w', '--wrap', action='store_true', default=WRAP)
+    p.add_argument('-d', '--damp-edge', type=int, default=DAMP_EDGE)
+    args = p.parse_args()
     try:
-        main()
+        main(args.width, args.height, wrap=args.wrap, damp_edge=args.damp_edge)
     finally:
         pg.quit()
