b7408db420376b28b632ef5372b4f00cc8b48703
Ryan McCampbell <rmccampbell7@gmail.com>
2020-05-21 22:30:11 -0400

    updated htmltable

--- htmltable.py
+++ htmltable.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 import sys, re, bs4, argparse, urllib.request
 
-def parse_table(html='-', id=None, number=None, columns=None, startrow=None,
-                endrow=None, tbody=False):
+def _parse_columns(columns):
+    if isinstance(columns, (list, tuple, range)):
+        return columns
+    if isinstance(columns, int):
+        return [columns]
+    column_list = []
+    for s in columns.split(','):
+        if '-' in s:
+            start, stop = map(int, s.split('-', 1))
+            column_list.extend(range(start, stop+1))
+        else:
+            column_list.append(int(s))
+    return column_list
+
+def parse_table(html='-', selector=None, number=None, columns=None,
+                startrow=None, endrow=None, tbody=False, duplicate_cells=False,
+                parser=None):
     if html == '-':
         html = sys.stdin
     elif re.match(r'\s*<', html):
@@ -11,18 +26,23 @@ def parse_table(html='-', id=None, number=None, columns=None, startrow=None,
         html = urllib.request.urlopen(html)
     else:
         html = open(html)
-    doc = bs4.BeautifulSoup(html, 'html5lib')
+    if parser is None:
+        for parser in ['lxml', 'html5lib', 'html.parser']:
+            if bs4.builder_registry.lookup(parser):
+                break
+    doc = bs4.BeautifulSoup(html, parser)
 
-    if id:
-        table = doc.find(id=id)
+    if selector:
+        table = doc.select_one(selector)
         if not table:
-            raise ValueError("id '{}' not found.".format(id))
+            raise ValueError("selector '{}' not found.".format(selector))
         if table.name != 'table':
             table = table.table or table
     elif number is not None:
         table = doc.find_all('table')[number]
     else:
         table = doc.table or doc.body or doc
+
     if tbody:
         table = table.tbody or table
     else:
@@ -30,12 +50,20 @@ def parse_table(html='-', id=None, number=None, columns=None, startrow=None,
             tag = table.find(tname)
             tag and tag.unwrap()
 
+    columns = columns and _parse_columns(columns)
+
     results = []
     for tr in table.find_all('tr', recursive=False)[startrow : endrow]:
-        tds = tr.find_all(('td', 'th'), recursive=False)
+        cells = []
+        for td in tr.find_all(('td', 'th'), recursive=False):
+            text = re.sub(r'[^\S\xa0]+', ' ', td.text).replace('\xa0', ' ')
+            cells.append(text)
+            if td.has_attr('colspan'):
+                repeat = int(td['colspan']) - 1
+                cells.extend([text if duplicate_cells else ''] * repeat)
         if columns:
-            tds = [tds[col] for col in columns if col < len(tds)]
-        results.append([re.sub(r'\s', ' ', td.text) for td in tds])
+            cells = [cells[col] for col in columns if col < len(cells)]
+        results.append(cells)
     return results
 
 def print_delimited(arr, delimiter='\t'):
@@ -54,17 +82,21 @@ def pretty_print(arr, pad=2):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('html', nargs='?', default='-')
-    parser.add_argument('-i', '--id')
+    parser.add_argument('-S', '--selector')
     parser.add_argument('-n', '--number', type=int)
-    parser.add_argument('-c', '--columns', type=int, nargs='+')
+    parser.add_argument('-c', '--columns', type=_parse_columns)
     parser.add_argument('-s', '--startrow', type=int)
     parser.add_argument('-e', '--endrow', type=int)
     parser.add_argument('-b', '--tbody', action='store_true')
+    parser.add_argument('-D', '--duplicate-cells', action='store_true')
     parser.add_argument('-d', '--delimiter')
     parser.add_argument('-t', '--tabs', dest='delimiter', const='\t',
                         action='store_const')
+    parser.add_argument('-P', '--pad', type=int, default=2)
+    parser.add_argument('-p', '--parser')
     kwargs = vars(parser.parse_args())
     delimiter = kwargs.pop('delimiter')
+    pad = kwargs.pop('pad')
     try:
         table = parse_table(**kwargs)
     except Exception as e:
@@ -72,4 +104,4 @@ if __name__ == '__main__':
     if delimiter:
         print_delimited(table, delimiter)
     else:
-        pretty_print(table)
+        pretty_print(table, pad)
