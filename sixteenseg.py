530483acdf22df14095d97460ab2a2ee7fd0babb
Ryan McCampbell <rmccampbell7@gmail.com>
2020-09-04 04:45:45 -0400

    updated sixteenseg

--- sixteenseg.py
+++ sixteenseg.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python3
+import re
 from itertools import zip_longest
 
 PATTERN = r'''
- _ _ 
-|\|/|
- - - 
-|/|\|
+ _ _  
+|\|/| 
+ - - .
+|/|\| 
  - - .
 '''[1:]
 
 TEMPLATE = '''\
- 0 1 
-789A2
- F B 
-6EDC3
+ 0 1  
+789A2 
+ F B H
+6EDC3 
  5 4 G
+'''
+
+PATTERN_A = r'''
+ _  _   
+|_\|_/|.
+|_/|_\|.
+'''[1:]
+
+TEMPLATE_A = '''\
+ 0  1   
+7F89BA2H
+65ED4C3G
 '''
 
 ASCII_TABLE = [
@@ -25,7 +38,7 @@ ASCII_TABLE = [
 #   0        1        2        3        4        5        6        7
     0x044ff, 0x0040c, 0x08877, 0x0083f, 0x0888c, 0x090b3, 0x088fb, 0x0000f,
 #   8        9        :        ;        <        =        >        ?
-    0x088ff, 0x088bf, 0x02200, 0x04200, 0x09400, 0x08830, 0x04900, 0x12807,
+    0x088ff, 0x088bf, 0x30000, 0x04200, 0x09400, 0x08830, 0x04900, 0x12807,
 #   @        A        B        C        D        E        F        G
     0x00af7, 0x088cf, 0x02a3f, 0x000f3, 0x0223f, 0x080f3, 0x080c3, 0x008fb,
 #   H        I        J        K        L        M        N        O
@@ -41,29 +54,53 @@ ASCII_TABLE = [
 #   p        q        r        s        t        u        v        w
     0x082c1, 0x0a281, 0x08040, 0x0a0a1, 0x080e0, 0x02060, 0x04040, 0x05048,
 #   x        y        z        {        |        }        ~
-    0x05500, 0x00a1c, 0x0c020, 0x0a212, 0x02200, 0x02a21, 0x08844, 0x00000,
+    0x05500, 0x00a1c, 0x0c020, 0x0a212, 0x02200, 0x02a21, 0x00A85, 0x00000,
 ]
 
+def ansi_len(s):
+    return len(re.sub(r'\x1b\[.*?m', '', s))
+
+def ansi_pad(s, w):
+    return s + ' '*(w - ansi_len(s))
+
 def hconcat(*strings):
-    line_lists = [s.splitlines() for s in strings]
-    widths = [max(map(len, lines)) for lines in line_lists]
+    # Reverse lines so zip_longest pads at top instead of bottom
+    line_lists = [s.splitlines()[::-1] for s in strings]
+    widths = [max(map(ansi_len, lines)) for lines in line_lists]
     return '\n'.join(
-        ''.join(line.ljust(w) for line, w in zip(row, widths))
-        for row in zip_longest(*line_lists, fillvalue='')
+        ''.join(ansi_pad(line, w) for line, w in zip(row, widths))
+        for row in reversed(list(zip_longest(*line_lists, fillvalue='')))
     )
 
-def decode(x):
+def decode(x, ansi=True):
     if hasattr(x, '__iter__') and not isinstance(x, str):
-        return hconcat(*(decode(y) for y in x))
-    return ''.join(
-        c if (not d.isalnum()) or (x >> int(d, 17) & 1) else ' '
-        for c, d in zip(PATTERN, TEMPLATE)
+        return hconcat(*(decode(y, ansi) for y in x))
+    pattern, template = (PATTERN_A, TEMPLATE_A) if ansi else (PATTERN, TEMPLATE)
+    s = ''.join(
+        c if (not d.isalnum()) or (x >> int(d, 18) & 1) else
+        '' if ansi and c == '_' else ' '
+        for c, d in zip(pattern, template)
     )
+    if ansi:
+        s = re.sub(r'_(.)', '\x1b[4m\\1\x1b[24m', s)
+    return s
 
-def sixteenseg(s):
-    if len(s) > 1:
-        return '\n'.join(hconcat(*map(sixteenseg, l)) for l in s.splitlines())
-    return decode(ASCII_TABLE[ord(s) - 0x20]) if ' ' <= s <= '\x7f' else s
+def sixteenseg(s, ansi=True):
+    rows = []
+    for line in s.splitlines():
+        row = []
+        for c in re.findall(r'(?=[ -\x7f])[^.:!?][.:]|.', line):
+            # Pass through non-ascii characters
+            if not ' ' <= c[0] <= '\x7f':
+                row.append(c)
+                continue
+            code = ASCII_TABLE[ord(c[0]) - 0x20]
+            # Combine . and : with previous character
+            if len(c) == 2:
+                code |= ASCII_TABLE[ord(c[1]) - 0x20]
+            row.append(decode(code, ansi))
+        rows.append(hconcat(*row))
+    return '\n'.join(rows)
 
 
 if __name__ == '__main__':
