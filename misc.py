c6f2d57babdb1831b7d690169931c852152c9c49
Ryan McCampbell <rmccampbell7@gmail.com>
2020-10-26 17:01:53 -0400

    updated misc

--- misc.py
+++ misc.py
@@ -317,16 +317,18 @@ MORSE_TABLE = {
     '8': '---..', '9': '----.', '.': '.-.-.-', ',': '--..--', '?': '..--..',
     "'": '.----.', '!': '-.-.--', '/': '-..-.', '(': '-.--.', ')': '-.--.-',
     '&': '.-...', ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.',
-    '-': '-....-', '_': '..--.-', '"': '.-..-.', '$': '...-..-', '@': '.--.-.'}
+    '-': '-....-', '_': '..--.-', '"': '.-..-.', '$': '...-..-', '@': '.--.-.',
+    ' ': ' ',
+}
 
 MORSE_TABLE_R = {v: k for k, v in MORSE_TABLE.items()}
 
 def morse_encode(s):
-    return ' '.join(MORSE_TABLE.get(c.upper(), '') for c in s)
+    return ' '.join(filter(None, (MORSE_TABLE.get(c.upper()) for c in s)))
 
 def morse_decode(s):
-    return ' '.join(''.join(MORSE_TABLE_R.get(c, c)
-                            for c in w.split()) for w in s.split('  '))
+    return ' '.join(''.join(MORSE_TABLE_R.get(c, c) for c in w.split())
+                    for w in s.split('  '))
 
 
 ASCII_SUBS_R = {
@@ -500,25 +502,35 @@ def smootherstep(x):
     return x**3 * (x * (x * 6 - 15) + 10);
 
 
+_ibm437_visible_table = {
+    0x01: 0x263A, 0x02: 0x263B, 0x03: 0x2665, 0x04: 0x2666,
+    0x05: 0x2663, 0x06: 0x2660, 0x07: 0x2022, 0x08: 0x25D8,
+    0x09: 0x25CB, 0x0a: 0x25D9, 0x0b: 0x2642, 0x0c: 0x2640,
+    0x0d: 0x266A, 0x0e: 0x266B, 0x0f: 0x263C, 0x10: 0x25BA,
+    0x11: 0x25C4, 0x12: 0x2195, 0x13: 0x203C, 0x14: 0x00B6,
+    0x15: 0x00A7, 0x16: 0x25AC, 0x17: 0x21A8, 0x18: 0x2191,
+    0x19: 0x2193, 0x1a: 0x2192, 0x1b: 0x2190, 0x1c: 0x221F,
+    0x1d: 0x2194, 0x1e: 0x25B2, 0x1f: 0x25BC, 0x7f: 0x2302,
+}
+_ibm437_visible_table_r = {v: k for k, v in _ibm437_visible_table.items()}
+
+def ibm437_visible_encode(b):
+    return b.translate(_ibm437_visible_table_r).encode('cp437')
+
 def ibm437_visible_decode(b):
-    return b.decode('ibm437').translate({
-        0x01: "\u263A", 0x02: "\u263B", 0x03: "\u2665", 0x04: "\u2666",
-        0x05: "\u2663", 0x06: "\u2660", 0x07: "\u2022", 0x08: "\u25D8",
-        0x09: "\u25CB", 0x0a: "\u25D9", 0x0b: "\u2642", 0x0c: "\u2640",
-        0x0d: "\u266A", 0x0e: "\u266B", 0x0f: "\u263C", 0x10: "\u25BA",
-        0x11: "\u25C4", 0x12: "\u2195", 0x13: "\u203C", 0x14: "\u00B6",
-        0x15: "\u00A7", 0x16: "\u25AC", 0x17: "\u21A8", 0x18: "\u2191",
-        0x19: "\u2193", 0x1a: "\u2192", 0x1b: "\u2190", 0x1c: "\u221F",
-        0x1d: "\u2194", 0x1e: "\u25B2", 0x1f: "\u25BC", 0x7f: "\u2302",
-    })
+    return b.decode('cp437').translate(_ibm437_visible_table)
 
 
-# codecs.register_error('fallback_latin1', decode_fallback_latin1)
-def decode_fallback_latin1(e):
+# codecs.register_error('fallback_latin1', codec_error_fallback_latin1)
+def codec_error_fallback_latin1(e):
+    if isinstance(e, UnicodeEncodeError):
+        return e.object[e.start:e.end].encode('latin-1'), e.end
     return e.object[e.start:e.end].decode('latin-1'), e.end
 
-# codecs.register_error('fallback_cp1252', decode_fallback_cp1252)
-def decode_fallback_cp1252(e):
+# codecs.register_error('fallback_cp1252', codec_error_fallback_cp1252)
+def codec_error_fallback_cp1252(e):
+    if isinstance(e, UnicodeEncodeError):
+        return e.object[e.start:e.end].encode('cp1252', 'fallback_latin1'), e.end
     return e.object[e.start:e.end].decode('cp1252', 'fallback_latin1'), e.end
 
 
@@ -537,3 +549,20 @@ def farey_approx(x, m=100):
         cn, cd = an + bn, ad + bd
         print((an,ad), (bn,bd), (cn,cd))
     return cn, cd
+
+
+def bin_encode(inds):
+    return sum([1 << i for i in inds])
+
+def bin_decode(x):
+    return [i for i in range(x.bit_length()) if x >> i & 1]
+
+
+def parse_time(string):
+    if string.count(':') == 2:
+        h, m, s = string.split(':')
+        return int(h)*3600 + int(m)*60 + float(s)
+    if ':' in string:
+        m, s = string.split(':')
+        return int(m)*60 + float(s)
+    return float(string)
