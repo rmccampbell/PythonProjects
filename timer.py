3723bf071a067832ddf3597c073606aac1bc101b
Ryan McCampbell <rmccampbell7@gmail.com>
2020-12-03 00:57:38 -0500

    updated timer.py

--- timer.py
+++ timer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys, time, argparse
 
-def timer(minutes, seconds=0, showsecs=False, msgbox=True):
+def timer(minutes, seconds=0, showsecs=True, msgbox=True):
     seconds += minutes * 60
     curtime = time.time()
     rtime = seconds
@@ -19,9 +19,11 @@ def timer(minutes, seconds=0, showsecs=False, msgbox=True):
                       end='', flush=True)
 
         print()
-        print("Time's Up!\a")
-        if msgbox:
+        print("Time's Up!")
+        if msgbox and show_msgbox:
             show_msgbox()
+        else:
+            print('\a', end='')
 
     except KeyboardInterrupt:
         print("\r{}:{:02} ".format(rtime // 60, rtime % 60))
@@ -32,15 +34,24 @@ try:
         win32ui.MessageBox("Time's Up!", "Timer", win32con.MB_SYSTEMMODAL |
                            win32con.MB_ICONWARNING)
 except ImportError:
-    def show_msgbox():
-        pass
+    show_msgbox = None
 
 if __name__ == '__main__':
     p = argparse.ArgumentParser()
-    p.add_argument('minutes', type=int)
+    p.add_argument('minutes')
     p.add_argument('seconds', nargs='?', default=0, type=int)
-    p.add_argument('-s', '--showsecs', action='store_true')
-    p.add_argument('-n', '--nomsgbox', action='store_false', dest='msgbox')
+    p.add_argument('-S', '--noshowsecs', dest='showsecs', action='store_false')
+    p.add_argument('-M', '--nomsgbox', dest='msgbox', action='store_false')
     args = p.parse_args()
+    if ':' in args.minutes:
+        try:
+            args.minutes, args.seconds = map(int, args.minutes.split(':'))
+        except ValueError as e:
+            p.error(e)
+    else:
+        try:
+            args.minutes = int(args.minutes)
+        except ValueError as e:
+            p.error(e)
     timer(args.minutes, args.seconds, showsecs=args.showsecs,
           msgbox=args.msgbox)
