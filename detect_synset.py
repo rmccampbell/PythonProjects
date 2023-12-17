94fb31fd593b21a95a2ac21b320695e345d88d64
Ryan McCampbell <rmccampbell7@gmail.com>
2023-12-17 01:41:34 -0800

    Updated detect_imagenet and added detect_synset

--- detect_synset.py
+++ detect_synset.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-import sys, os, json, argparse
+import sys, os, json, argparse, warnings
 from urllib.parse import urlsplit
 from urllib.request import urlopen, Request
 import torch
 import torchvision.transforms as transforms
 from torchvision.models import resnet50, ResNet50_Weights
 from PIL import Image
+from nltk.corpus import wordnet
 
 USER_AGENT = 'dummy_user_agent'
 
@@ -46,15 +47,36 @@ def detect(image, k=10):
                 zip(topind.tolist(), topprob.tolist())]
     return clsprobs
 
+def in_hypernym_closure(synset, target):
+    with warnings.catch_warnings(action='ignore'):
+        return (synset == target or
+                target in synset.closure(lambda s: s.hypernyms()))
+
+def detect_synset(image, synset, k=1):
+    if '.' in synset:
+        target = wordnet.synset(synset)
+    else:
+        target = wordnet.synsets(synset, pos=wordnet.NOUN)[0]
+    detections = detect(image, k)
+    match = False
+    tot_prob = 0
+    for cls, id, prob in detections:
+        ss = wordnet.synset_from_pos_and_offset(id[0], int(id[1:]))
+        if in_hypernym_closure(ss, target):
+            match = True
+            tot_prob += prob
+    return match, tot_prob
+
 if __name__ == '__main__':
     p = argparse.ArgumentParser()
-    p.add_argument('image')
-    p.add_argument('-k', '--topk', type=int, default=10)
-    p.add_argument('-i', '--id', action='store_true')
+    p.add_argument('image', nargs='+')
+    p.add_argument('-s', '--synset', default='bird.n.01')
+    p.add_argument('-k', '--topk', type=int, default=1)
+    p.add_argument('-p', '--prob', action='store_true')
     args = p.parse_args()
-    clsprobs = detect(args.image, args.topk)
-    for cls, id, prob in clsprobs:
-        if args.id:
-            print(f'{cls} ({id}):\t{prob:.2%}')
+    for image in args.image:
+        match, prob = detect_synset(image, args.synset, args.topk)
+        if match:
+            print(f'Yes: {prob:%}' if args.prob else 'Yes')
         else:
-            print(f'{cls}:\t{prob:.2%}')
+            print('No')
