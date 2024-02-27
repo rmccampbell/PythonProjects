11297ddf8232cc99ff780005c03495ae91348f28
Ryan McCampbell <rmccampbell7@gmail.com>
2024-02-26 17:20:04 -0800

    support raw bytes in args.py

--- args.py
+++ args.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
-import sys
+import sys, os
+
+byte = len(sys.argv) > 1 and sys.argv[1] == '-b'
+if byte:
+    sys.argv.remove('-b')
+
 for arg in sys.argv[1:]:
-    print(repr(arg))
+    if byte:
+        print(repr(os.fsencode(arg)).lstrip('b'))
+    else:
+        print(repr(arg))
