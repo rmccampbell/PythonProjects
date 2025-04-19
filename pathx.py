c3be6e666259bead9bb478b5038520de7caf6838
Ryan McCampbell <rmccampbell7@gmail.com>
2025-04-19 16:09:51 -0700

    updated pathx

--- pathx.py
+++ pathx.py
@@ -4,9 +4,9 @@ import os.path as osp
 from winreg import *
 from ctypes.wintypes import *
 
-# SendMessage = ctypes.windll.user32.SendMessageW
 SendMessageTimeout = ctypes.windll.user32.SendMessageTimeoutW
-SendMessageTimeout.argtypes = [HWND, UINT, WPARAM, ctypes.c_wchar_p, UINT, UINT, PDWORD]
+SendMessageTimeout.argtypes = [
+    HWND, UINT, WPARAM, ctypes.c_wchar_p, UINT, UINT, ctypes.c_void_p]
 SendMessageTimeout.restype = ctypes.c_ssize_t
 HWND_BROADCAST = 0xffff
 WM_SETTINGCHANGE = 0x1a
@@ -49,7 +49,6 @@ def pathx(add=None, index=None, remove=None, contains=None, system=False,
         if modifying:
             typ = REG_EXPAND_SZ if value.count('%') >= 2 else REG_SZ
             SetValueEx(key, 'Path', 0, typ, value)
-            # SendMessage(HWND_BROADCAST, WM_SETTINGCHANGE, 0, 'Environment')
             SendMessageTimeout(HWND_BROADCAST, WM_SETTINGCHANGE, 0,
                                'Environment', SMTO_NORMAL, 100, None)
             print('** Registry updated **', file=sys.stderr)
